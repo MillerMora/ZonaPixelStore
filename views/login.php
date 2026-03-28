bd6bf39927d60e9a58c17ca76cf8eaaf95c167f5
Miller Mora <millermora2022@gmail.com>
2026-03-28 11:26:15 -0500

    se agrego el inicio de sesion y se añadio a todos los archivos la constante  __dir__

--- views/login.php
+++ views/login.php
@@ -48,14 +48,14 @@ session_start();
             <label>Usuario o correo</label>
             <div class="input-icon-wrap">
               <i class="fas fa-user"></i>
-              <input type="text" class="form-input" name="email" placeholder="tu@correo.com" />
+              <input type="text" class="form-input" name="login" placeholder="tu@correo.com" />
             </div>
           </div>
           <div class="form-field">
             <label>Contraseña</label>
             <div class="input-icon-wrap">
               <i class="fas fa-lock"></i>
-              <input type="password" class="form-input" placeholder="Tu contraseña" />
+              <input type="password" class="form-input" name="password" placeholder="Tu contraseña" />
             </div>
           </div>
 
--- views/php/usuarios/login/login.php
+++ views/php/usuarios/login/login.php
@@ -1,39 +1,22 @@
-<?php 
+<?php
 include_once "../usuarioModel.php";
 session_start();
 
-function iniciar_sesion(){
-    if ((isset($_POST['email']) || isset($_POST['username'])) && isset($_POST['password'])){
-        if ($_POST['email'] != null){
-            $consulta = consultar_usuarios_correo($_POST['email']);
-            if (!$consulta){
+function iniciar_sesion($usuario, $password)
+{
+    if (isset($usuario) && isset($password)) {
+        if (filter_var($usuario, FILTER_VALIDATE_EMAIL)) {
+            $consulta = consultar_usuarios_correo($usuario);
+            if (!$consulta) {
                 echo 'correo incorrecto';
                 return;
             }
-            if ($consulta['password_hash'] != $_POST ['passowrd']) {
+            if ($consulta['password_hash'] != $password) {
                 echo 'contraseña incorrecta';
                 return;
             }
 
-            $_SESSION['rol'] = $consulta['rol'];
-            $_SESSION['nombre'] = $consulta['nombre'];
-            $_SESSION['email'] = $consulta['email'];
-            $_SESSION['username'] = $consulta['username'];
-            $_SESSION['password'] = $consulta['password_hash'];
-            echo 'inicio de sesion exitoso';
-        }
-        if ($_POST['username'] != null){
-            $consulta = consultar_usuarios_nombreUsuario($_POST['username']);
-            if (!$consulta){
-                echo 'nombre de usuario incorrecto';
-                return;
-            }
-            if ($consulta['password_hash'] != $_POST ['passowrd']) {
-                echo 'contraseña incorrecta';
-                return;
-            }
-
-            $_SESSION['rol'] = $consulta['rol'];
+            $_SESSION['rol'] = $consulta['rol_id'];
             $_SESSION['nombre'] = $consulta['nombre'];
             $_SESSION['email'] = $consulta['email'];
             $_SESSION['username'] = $consulta['username'];
@@ -41,12 +24,37 @@ function iniciar_sesion(){
             echo 'inicio de sesion exitoso';
             return;
         }
+
+
+        $consulta = consultar_usuarios_nombreUsuario($usuario);
+        if (!$consulta) {
+            echo 'nombre de usuario incorrecto';
+            return;
+        }
+        if ($consulta['password_hash'] != $password) {
+            echo 'contraseña incorrecta';
+            return;
+        }
+
+        $_SESSION['rol'] = $consulta['rol_id'];
+        $_SESSION['nombre'] = $consulta['nombre'];
+        $_SESSION['email'] = $consulta['email'];
+        $_SESSION['username'] = $consulta['username'];
+        $_SESSION['password'] = $consulta['password_hash'];
+        echo 'inicio de sesion exitoso';
+        return;
     }
     echo 'error datos nulos';
     return;
 }
 
-if (isset($_GET['iniciar'])){
-    iniciar_sesion();
+function cerrar_sesion (){
+
+    session_unset();
+
+    session_destroy();
+
 }
-?>
+if (isset($_GET['iniciar'])) {
+    iniciar_sesion($usuario= $_POST['login'], $password = $_POST['password']);
+}

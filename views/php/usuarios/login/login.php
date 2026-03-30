7ee501e746c1ca2f39b2b830f22832852a693294
Miller Mora <millermora2022@gmail.com>
2026-03-29 22:43:01 -0500

    mejora la estructura de validación en el login y el manejo de mensajes de error

--- views/php/usuarios/login/login.php
+++ views/php/usuarios/login/login.php
@@ -4,47 +4,38 @@ session_start();
 
 function iniciar_sesion($usuario, $password)
 {
-    if (isset($usuario) && isset($password)) {
-        if (filter_var($usuario, FILTER_VALIDATE_EMAIL)) {
-            $consulta = consultar_usuarios_correo($usuario);
-            if (!$consulta) {
-                echo 'correo incorrecto';
-                return header('location: ../../../../index.php');
-            }
-            if ($consulta['password_hash'] != $password) {
-                echo 'contraseña incorrecta';
-                return header('location: ../../../../index.php');
-            }
-
-            $_SESSION['rol'] = $consulta['rol_id'];
-            $_SESSION['nombre'] = $consulta['nombre'];
-            $_SESSION['email'] = $consulta['email'];
-            $_SESSION['username'] = $consulta['username'];
-            $_SESSION['password'] = $consulta['password_hash'];
-            echo 'inicio de sesion exitoso';
-            return header('location: ../../../../index.php');
-        }
-
-
-        $consulta = consultar_usuarios_nombreUsuario($usuario);
-        if (!$consulta) {
-            echo 'nombre de usuario incorrecto';
-            return header('location: ../../../../index.php') ;
-        }
-        if ($consulta['password_hash'] != $password) {
-            echo 'contraseña incorrecta';
-            return header('location: ../../../../index.php');
-        }
-
-        $_SESSION['rol'] = $consulta['rol_id'];
-        $_SESSION['nombre'] = $consulta['nombre'];
-        $_SESSION['email'] = $consulta['email'];
-        $_SESSION['username'] = $consulta['username'];
-        $_SESSION['password'] = $consulta['password_hash'];
-        echo 'inicio de sesion exitoso';
+    if (empty($usuario) || empty($password)) {
+        $_SESSION['index_login_error'] = 'Error en el sistema de login. Campos vacíos. Intenta más tarde.';
         return header('location: ../../../../index.php');
     }
-    echo 'error datos nulos';
+
+    if (filter_var($usuario, FILTER_VALIDATE_EMAIL)) {
+        $consulta = consultar_usuarios_correo($usuario);
+        if (!$consulta) {
+            $_SESSION['login_error'] = 'Usuario o contraseña incorrectos.';
+            return header('location: ../../../login.php');
+        }
+        if ($consulta['password_hash'] != $password) {
+            $_SESSION['login_error'] = 'Usuario o contraseña incorrectos.';
+            return header('location: ../../../login.php');
+        }
+    } else {
+        $consulta = consultar_usuarios_nombreUsuario($usuario);
+        if (!$consulta) {
+            $_SESSION['login_error'] = 'Usuario o contraseña incorrectos.';
+            return header('location: ../../../login.php');
+        }
+        if ($consulta['password_hash'] != $password) {
+            $_SESSION['login_error'] = 'Usuario o contraseña incorrectos.';
+            return header('location: ../../../login.php');
+        }
+    }
+
+    $_SESSION['rol'] = $consulta['rol_id'];
+    $_SESSION['nombre'] = $consulta['nombre'];
+    $_SESSION['email'] = $consulta['email'];
+    $_SESSION['username'] = $consulta['username'];
+    $_SESSION['password'] = $consulta['password_hash'];
     return header('location: ../../../../index.php');
 }
 
@@ -58,7 +49,7 @@ function cerrar_sesion (){
 
 }
 if (isset($_GET['iniciar'])) {
-    iniciar_sesion($usuario= $_POST['login'], $password = $_POST['password']);
+    iniciar_sesion($_POST['login'], $_POST['password']);
 }
 if (isset($_GET['cerrar'])) {
     cerrar_sesion();

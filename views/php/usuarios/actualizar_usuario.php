614e0ca1b802f9d785f5d5ccde2696407010e232
Miller Mora <millermora2022@gmail.com>
2026-03-29 22:38:12 -0500

    añade el llamado a la funcion de actualizar_contraseña

--- views/php/usuarios/actualizar_usuario.php
+++ views/php/usuarios/actualizar_usuario.php
@@ -5,32 +5,54 @@ include "./usuarioModel.php";
 if (isset($_POST['id_usuario'])){
     $id = $_POST['id_usuario'];
 }
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$usuario = $_POST['username'];
-$correo = $_POST['email'];
-if ($_SESSION['rol'] === 1 || $_SESSION['rol'] === 'admin' ||($_POST['password'] === $_POST['confirm_password']) && (strlen($_POST['password']) >= 8 && strlen($_POST['confirm_password']) >= 8  ) ){
+if (isset($_POST['email']) && isset($_POST['password'])){
     $contrasena = $_POST['password'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $usuario = $_POST['username'];
+    $correo = $_POST['email'];
+    $id_rol = $_POST['rol_id'] ?? 2;
 }
-$id_rol = $_POST['rol_id'] ?? 2;
+if (isset($_POST['new_password']) && isset($_POST['confirm_password']) ){
 
+    if ($_POST['new_password'] < 8){
+        $_SESSION['password_error'] = 'la nueva contraseña debe ser mayor de 8 digitos';
+        header('location: ./cambiar_contraseña.php');
+        exit();
+    }
+    if ($_POST['new_password'] === $_POST['confirm_password']){
+        $usuario = $_SESSION['username'];
+        $contrasena = $_POST['new_password'];
+    }
+    else {
+        $_SESSION['password_error'] = 'Ambas contraseñas no coinciden';
+        header('location: ./cambiar_contraseña.php');
+        exit();
+    }
 
-$succes;
+}
+
 if (isset($_GET['crear'])){
     $crear_datos = crear_usuario_rol($nombre, $apellido, $usuario, $correo, $contrasena, $id_rol);
     $succes = $crear_datos;
 } elseif(isset($id)){
     $actualizar_datos = actualizar_usuario($id, $nombre, $apellido, $usuario, $correo, $contrasena, $id_rol);
     $succes = $actualizar_datos;
+} elseif ($contrasena) {
+    $actualizar_datos = actualizar_contraseña($usuario,$contrasena);
+    if ($actualizar_datos){
+        header('location: ./editar_usuario_cliente.php');
+        exit();
+    }
 }
 
 if ($succes) {
-    if ($_SESSION['rol'] === 1 || $_SESSION['rol'] === 'admin'){
+    if ($_SESSION['rol'] === 1){
         header('Location: usuario.php');
-        exit;
+        exit();
     }
     header('Location: ../../login.php');
-    exit;
+    exit();
 } else {
     echo "Error al actualizar el usuario";
 }
